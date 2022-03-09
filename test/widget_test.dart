0a832f62a8bf368c1030e181d4fb007066a46785
The Tai Manh <84488032+ttaidev@users.noreply.github.com>
2022-03-09 22:17:55 +0700

    up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:push_noti/main.dart';
+import 'package:Send-push_notification/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
