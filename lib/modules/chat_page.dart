2ba045bb7da187abcca7971af4ed5848ada5f3d7
The Tai Manh <84488032+ttaidev@users.noreply.github.com>
2022-03-29 00:40:10 +0700

    up

--- lib/modules/chat_page.dart
+++ lib/modules/chat_page.dart
@@ -3,21 +3,53 @@ import 'package:chat_app/models/chat_users.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatefulWidget{
+class ChatPage extends StatefulWidget {
   @override
   _ChatPageState createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
   List<ChatUsers> chatUsers = [
-    ChatUsers(text: "Jane Russel", secondaryText: "Awesome Setup", image: "images/userImage1.jpeg", time: "Now"),
-    ChatUsers(text: "Glady's Murphy", secondaryText: "That's Great", image: "images/userImage2.jpeg", time: "Yesterday"),
-    ChatUsers(text: "Jorge Henry", secondaryText: "Hey where are you?", image: "images/userImage3.jpeg", time: "31 Mar"),
-    ChatUsers(text: "Philip Fox", secondaryText: "Busy! Call me in 20 mins", image: "images/userImage4.jpeg", time: "28 Mar"),
-    ChatUsers(text: "Debra Hawkins", secondaryText: "Thankyou, It's awesome", image: "images/userImage5.jpeg", time: "23 Mar"),
-    ChatUsers(text: "Jacob Pena", secondaryText: "will update you in evening", image: "images/userImage6.jpeg", time: "17 Mar"),
-    ChatUsers(text: "Andrey Jones", secondaryText: "Can you please share the file?", image: "images/userImage7.jpeg", time: "24 Feb"),
-    ChatUsers(text: "John Wick", secondaryText: "How are you?", image: "images/userImage8.jpeg", time: "18 Feb"),
+    ChatUsers(
+        text: "Mạnh Thế Tài",
+        secondaryText: "Oke bạn nha!",
+        image: "images/userImage1.jpeg",
+        time: "Now"),
+    ChatUsers(
+        text: "Hello",
+        secondaryText: "That's Great",
+        image: "images/userImage2.jpeg",
+        time: "Yesterday"),
+    ChatUsers(
+        text: "Jorge Henry",
+        secondaryText: "Hey where are you?",
+        image: "images/userImage3.jpeg",
+        time: "31 Mar"),
+    ChatUsers(
+        text: "Philip Fox",
+        secondaryText: "Busy! Call me in 20 mins",
+        image: "images/userImage4.jpeg",
+        time: "28 Mar"),
+    ChatUsers(
+        text: "Debra Hawkins",
+        secondaryText: "Thankyou, It's awesome",
+        image: "images/userImage5.jpeg",
+        time: "23 Mar"),
+    ChatUsers(
+        text: "Jacob Pena",
+        secondaryText: "will update you in evening",
+        image: "images/userImage6.jpeg",
+        time: "17 Mar"),
+    ChatUsers(
+        text: "Andrey Jones",
+        secondaryText: "Can you please share the file?",
+        image: "images/userImage7.jpeg",
+        time: "24 Feb"),
+    ChatUsers(
+        text: "John Wick",
+        secondaryText: "How are you?",
+        image: "images/userImage8.jpeg",
+        time: "18 Feb"),
   ];
   @override
   Widget build(BuildContext context) {
@@ -29,13 +61,18 @@ class _ChatPageState extends State<ChatPage> {
           children: <Widget>[
             SafeArea(
               child: Padding(
-                padding: EdgeInsets.only(left: 16,right: 16,top: 10),
+                padding: EdgeInsets.only(left: 16, right: 16, top: 10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text("Chats",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                    Text(
+                      "Chats",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
                     Container(
-                      padding: EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
+                      padding:
+                          EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
                       height: 30,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(30),
@@ -43,9 +80,19 @@ class _ChatPageState extends State<ChatPage> {
                       ),
                       child: Row(
                         children: <Widget>[
-                          Icon(Icons.add,color: Colors.pink,size: 20,),
-                          SizedBox(width: 2,),
-                          Text("New",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                          Icon(
+                            Icons.add,
+                            color: Colors.pink,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: 2,
+                          ),
+                          Text(
+                            "New",
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
                         ],
                       ),
                     )
@@ -54,21 +101,22 @@ class _ChatPageState extends State<ChatPage> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 16,left: 16,right: 16),
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
               child: TextField(
                 decoration: InputDecoration(
                   hintText: "Search...",
                   hintStyle: TextStyle(color: Colors.grey.shade400),
-                  prefixIcon: Icon(Icons.search,color: Colors.grey.shade400,size: 20,),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade400,
+                    size: 20,
+                  ),
                   filled: true,
                   fillColor: Colors.grey.shade100,
                   contentPadding: EdgeInsets.all(8),
                   enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
-                          color: Colors.grey.shade100
-                      )
-                  ),
+                      borderSide: BorderSide(color: Colors.grey.shade100)),
                 ),
               ),
             ),
@@ -77,13 +125,13 @@ class _ChatPageState extends State<ChatPage> {
               shrinkWrap: true,
               padding: EdgeInsets.only(top: 16),
               physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return ChatUsersList(
                   text: chatUsers[index].text,
                   secondaryText: chatUsers[index].secondaryText,
                   image: chatUsers[index].image,
                   time: chatUsers[index].time,
-                  isMessageRead: (index == 0 || index == 3)?true:false,
+                  isMessageRead: (index == 0 || index == 3) ? true : false,
                 );
               },
             ),
